e784c3902f236266414d11e70f29c19618f9d790
omoloye13 <omoloyefauziyat@gmail.com>
2024-05-04 21:50:50 +0100

    added stream chat sdk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,11 @@
 // import { View, Text } from 'react-native';
 import React from 'react';
 import { Slot, Stack } from 'expo-router';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function RootLayout() {
-	return <Slot />;
+	return (
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Slot />
+		</GestureHandlerRootView>
+	);
 }
