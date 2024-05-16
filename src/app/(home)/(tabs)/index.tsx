10200314da60b1d502843afa5707ed490365cebe
omoloye13 <omoloyefauziyat@gmail.com>
2024-05-16 21:57:23 +0100

    connecting the user avatar to stream chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -3,9 +3,12 @@ import React, { useState } from 'react';
 import { router } from 'expo-router';
 import { ChannelList } from 'stream-chat-expo';
 import { Channel as ChannelType, StreamChat } from 'stream-chat';
+import { useAuth } from '../../../providers/AuthProvider';
 export default function MainTabScreen() {
+	const { user } = useAuth();
 	return (
 		<ChannelList
+			filters={{ members: { $in: [user.id] } }}
 			onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
 		/>
 	);
