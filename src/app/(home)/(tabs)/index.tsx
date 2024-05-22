8424b33bc03e48c6e45a4293e981ecdd19d9ddbb
omoloye13 <omoloyefauziyat@gmail.com>
2024-05-22 14:11:04 +0100

    List of users

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,15 +1,32 @@
 import { View, Text } from 'react-native';
 import React, { useState } from 'react';
-import { router } from 'expo-router';
+import { Link, Stack, router } from 'expo-router';
 import { ChannelList } from 'stream-chat-expo';
 import { Channel as ChannelType, StreamChat } from 'stream-chat';
 import { useAuth } from '../../../providers/AuthProvider';
+import { FontAwesome5 } from '@expo/vector-icons';
 export default function MainTabScreen() {
 	const { user } = useAuth();
 	return (
-		<ChannelList
-			filters={{ members: { $in: [user.id] } }}
-			onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-		/>
+		<>
+			<Stack.Screen
+				options={{
+					headerRight: () => (
+						<Link href={'/(home)/users'} asChild>
+							<FontAwesome5
+								name='users'
+								size={22}
+								color='black'
+								style={{ marginHorizontal: 15 }}
+							/>
+						</Link>
+					),
+				}}
+			/>
+			<ChannelList
+				filters={{ members: { $in: [user.id] } }}
+				onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+			/>
+		</>
 	);
 }
